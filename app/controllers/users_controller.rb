de7835622734fdda78cbc8cfc750d9052d633ce3
Zeel Bhavsar <zeel.bhavsar@bacancytechnology.com>
2020-02-26 18:22:15 +0530

    Added approval functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_house, only: [:edit, :update]
   def home
-    @houses = House.joins(:address,:user).select('houses.*,addresses.*,users.*')
+    @houses = House.joins(:address,:user).select('houses.*,addresses.*,users.*').where("houses.approved=?",true)
+    
   end
 
   def index
@@ -22,5 +24,23 @@ class UsersController < ApplicationController
     
     render :home, :object => @houses
     
+  end
+
+  def approval
+    @approval = House.joins(:address,:user).select('houses.*,addresses.*,users.*').where("houses.approved=?",false)
+
+  end
+
+  
+
+  def update
+    @approval.approved = true
+    render :approval, :object => @approval
+  end
+  
+  private
+
+  def set_house
+      @approval = House.find(params[:id])
   end
 end
